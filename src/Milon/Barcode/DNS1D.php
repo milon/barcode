06d4fc0cb50737942637b362ef0b61e20fd2d5bf
Nuruzzaman Milon <contact@milon.im>
2020-08-31 17:36:33 +0200

    fix version 6

--- src/Milon/Barcode/DNS1D.php
+++ src/Milon/Barcode/DNS1D.php
@@ -85,7 +85,7 @@ class DNS1D {
      * @return string SVG code.
      * @protected
      */
-    protected function getBarcodeSVG($code, $type, $w = 2, $h = 30, $color = 'black', $showCode = true, $inline = false) {
+    public function getBarcodeSVG($code, $type, $w = 2, $h = 30, $color = 'black', $showCode = true, $inline = false) {
         if (!$this->store_path) {
             $this->setStorPath(app('config')->get("barcode.store_path"));
         }
@@ -131,7 +131,7 @@ class DNS1D {
      * @return string HTML code.
      * @protected
      */
-    protected function getBarcodeHTML($code, $type, $w = 2, $h = 30, $color = 'black', $showCode =false) {
+    public function getBarcodeHTML($code, $type, $w = 2, $h = 30, $color = 'black', $showCode =false) {
         if (!$this->store_path) {
             $this->setStorPath(app('config')->get("barcode.store_path"));
         }
@@ -169,7 +169,7 @@ class DNS1D {
      * @return image or false in case of error.
      * @protected
      */
-    protected function getBarcodePNG($code, $type, $w = 2, $h = 30, $color = array(0, 0, 0), $showCode = false) {
+    public function getBarcodePNG($code, $type, $w = 2, $h = 30, $color = array(0, 0, 0), $showCode = false) {
         if (!$this->store_path) {
             $this->setStorPath(app('config')->get("barcode.store_path"));
         }
@@ -245,7 +245,7 @@ class DNS1D {
      *
      * @return array
     */
-    protected function getBarcodeArray()
+    public function getBarcodeArray()
     {
         return $this->barcode_array;
     }
@@ -315,7 +315,7 @@ class DNS1D {
                 imagestring($png, 3, ($width/2) - ($width_text/2) , ($height - $height_text) , $code, $fgcol);
             }
 
-        $file_name= Str::slug($code);
+        $file_name= Str::slug($code.$type);
         $save_file = $this->checkfile($this->store_path . $file_name . ".png");
 
         if ($imagick) {
@@ -568,7 +568,7 @@ class DNS1D {
                 } else {
                     $t = false; // space
                 }
-                $w = $chr[$char]{$j};
+                $w = $chr[$char][$j];
                 $bararray['bcode'][$k] = array('t' => $t, 'w' => $w, 'h' => 1, 'p' => 0);
                 $bararray['maxw'] += $w;
                 ++$k;
@@ -771,7 +771,7 @@ class DNS1D {
                 } else {
                     $t = false; // space
                 }
-                $w = $chr[$char]{$j};
+                $w = $chr[$char][$j];
                 $bararray['bcode'][$k] = array('t' => $t, 'w' => $w, 'h' => 1, 'p' => 0);
                 $bararray['maxw'] += $w;
                 ++$k;
@@ -971,7 +971,7 @@ class DNS1D {
         $k = 0;
         for ($i = 0; $i < $len; ++$i) {
             $w += 1;
-            if (($i == ($len - 1)) OR (($i < ($len - 1)) AND ($seq[$i] != $seq{($i + 1)}))) {
+            if (($i == ($len - 1)) OR (($i < ($len - 1)) AND ($seq[$i] != $seq[($i + 1)]))) {
                 if ($seq[$i] == '1') {
                     $t = true; // bar
                 } else {
@@ -1024,7 +1024,7 @@ class DNS1D {
         $clen = strlen($code);
         for ($i = 0; $i < $clen; $i = ($i + 2)) {
             $char_bar = $code[$i];
-            $char_space = $code{$i + 1};
+            $char_space = $code[$i + 1];
             if ((!isset($chr[$char_bar])) OR (!isset($chr[$char_space]))) {
                 // invalid character
                 return false;
@@ -1033,7 +1033,7 @@ class DNS1D {
             $seq = '';
             $chrlen = strlen($chr[$char_bar]);
             for ($s = 0; $s < $chrlen; $s++) {
-                $seq .= $chr[$char_bar]{$s} . $chr[$char_space]{$s};
+                $seq .= $chr[$char_bar][$s] . $chr[$char_space][$s];
             }
             $seqlen = strlen($seq);
             for ($j = 0; $j < $seqlen; ++$j) {
@@ -1042,7 +1042,7 @@ class DNS1D {
                 } else {
                     $t = false; // space
                 }
-                $w = $seq{$j};
+                $w = $seq[$j];
                 $bararray['bcode'][$k] = array('t' => $t, 'w' => $w, 'h' => 1, 'p' => 0);
                 $bararray['maxw'] += $w;
                 ++$k;
@@ -1218,7 +1218,7 @@ class DNS1D {
                 }
             case 'C': { // MODE C
                     $startid = 105;
-                    if (ord($code{0}) == 241) {
+                    if (ord($code[0]) == 241) {
                         $code_data[] = 102;
                         $code = substr($code, 1);
                         --$len;
@@ -1228,7 +1228,7 @@ class DNS1D {
                         return false;
                     }
                     for ($i = 0; $i < $len; $i+=2) {
-                        $chrnum = $code[$i] . $code{$i + 1};
+                        $chrnum = $code[$i] . $code[$i + 1];
                         if (preg_match('/([0-9]{2})/', $chrnum) > 0) {
                             $code_data[] = intval($chrnum);
                         } else {
@@ -1296,7 +1296,7 @@ class DNS1D {
                                 }
                             case 'B': {
                                     if ($key == 0) {
-                                        $tmpchr = ord($seq[1]{0});
+                                        $tmpchr = ord($seq[1][0]);
                                         if (($seq[2] == 1) AND ($tmpchr >= 241) AND ($tmpchr <= 244) AND isset($sequence[($key + 1)]) AND ($sequence[($key + 1)][0] != 'B')) {
                                             switch ($sequence[($key + 1)][0]) {
                                                 case 'A': {
@@ -1344,7 +1344,7 @@ class DNS1D {
                                         $code_data[] = 99;
                                     }
                                     for ($i = 0; $i < $seq[2]; $i+=2) {
-                                        $chrnum = $seq[1][$i] . $seq[1]{$i + 1};
+                                        $chrnum = $seq[1][$i] . $seq[1][$i + 1];
                                         $code_data[] = intval($chrnum);
                                     }
                                     break;
@@ -1375,7 +1375,7 @@ class DNS1D {
                 } else {
                     $t = false; // space
                 }
-                $w = $seq{$j};
+                $w = $seq[$j];
                 $bararray['bcode'][] = array('t' => $t, 'w' => $w, 'h' => 1, 'p' => 0);
                 $bararray['maxw'] += $w;
             }
@@ -1468,8 +1468,8 @@ class DNS1D {
         if ($code_len == $data_len) {
             // add check digit
             $code .= $r;
-        } elseif ($r !== intval($code{$data_len})) {
-			throw new \Milon\Barcode\WrongCheckDigitException($r, intval($code{$data_len}));
+        } elseif ($r !== intval($code[$data_len])) {
+			throw new \Milon\Barcode\WrongCheckDigitException($r, intval($code[$data_len]));
         }
         if ($len == 12) {
             // UPC-A
@@ -1576,7 +1576,7 @@ class DNS1D {
         $seq = '101'; // left guard bar
         if ($upce) {
             $bararray = array('code' => $upce_code, 'maxw' => 0, 'maxh' => 1, 'bcode' => array());
-            $p = $upce_parities[$code{1}][$r];
+            $p = $upce_parities[$code[1]][$r];
             for ($i = 0; $i < 6; ++$i) {
                 $seq .= $codes[$p[$i]][$upce_code[$i]];
             }
@@ -1589,14 +1589,14 @@ class DNS1D {
                     $seq .= $codes['A'][$code[$i]];
                 }
             } else {
-                $p = $parities[$code{0}];
+                $p = $parities[$code[0]];
                 for ($i = 1; $i < $half_len; ++$i) {
                     $seq .= $codes[$p[$i - 1]][$code[$i]];
                 }
             }
             $seq .= '01010'; // center guard bar
             for ($i = $half_len; $i < $len; ++$i) {
-                $seq .= $codes['C'][$code{intval($i)}];
+                $seq .= $codes['C'][$code[intval($i)]];
             }
             $seq .= '101'; // right guard bar
         }
@@ -1604,7 +1604,7 @@ class DNS1D {
         $w = 0;
         for ($i = 0; $i < $clen; ++$i) {
             $w += 1;
-            if (($i == ($clen - 1)) OR (($i < ($clen - 1)) AND ($seq[$i] != $seq{($i + 1)}))) {
+            if (($i == ($clen - 1)) OR (($i < ($clen - 1)) AND ($seq[$i] != $seq[($i + 1)]))) {
                 if ($seq[$i] == '1') {
                     $t = true; // bar
                 } else {
@@ -1635,7 +1635,7 @@ class DNS1D {
         if ($len == 2) {
             $r = $code % 4;
         } elseif ($len == 5) {
-            $r = (3 * ($code{0} + $code{2} + $code{4})) + (9 * ($code{1} + $code{3}));
+            $r = (3 * ($code[0] + $code[2] + $code[4])) + (9 * ($code[1] + $code[3]));
             $r %= 10;
         } else {
             return false;
@@ -1686,7 +1686,7 @@ class DNS1D {
         );
         $p = $parities[$len][$r];
         $seq = '1011'; // left guard bar
-        $seq .= $codes[$p[0]][$code{0}];
+        $seq .= $codes[$p[0]][$code[0]];
         for ($i = 1; $i < $len; ++$i) {
             $seq .= '01'; // separator
             $seq .= $codes[$p[$i]][$code[$i]];
@@ -1967,7 +1967,7 @@ class DNS1D {
                 } else {
                     $t = false; // space
                 }
-                $w = $seq{$j};
+                $w = $seq[$j];
                 $bararray['bcode'][$k] = array('t' => $t, 'w' => $w, 'h' => 1, 'p' => 0);
                 $bararray['maxw'] += $w;
                 ++$k;
@@ -2058,7 +2058,7 @@ class DNS1D {
                 } else {
                     $t = false; // space
                 }
-                $w = $seq{$j};
+                $w = $seq[$j];
                 $bararray['bcode'][$k] = array('t' => $t, 'w' => $w, 'h' => 1, 'p' => 0);
                 $bararray['maxw'] += $w;
                 ++$k;
@@ -2198,9 +2198,9 @@ class DNS1D {
                 }
         }
         $binary_code = bcmul($binary_code, 10);
-        $binary_code = bcadd($binary_code, $tracking_number{0});
+        $binary_code = bcadd($binary_code, $tracking_number[0]);
         $binary_code = bcmul($binary_code, 5);
-        $binary_code = bcadd($binary_code, $tracking_number{1});
+        $binary_code = bcadd($binary_code, $tracking_number[1]);
         $binary_code .= substr($tracking_number, 2, 18);
         // convert to hexadecimal
         $binary_code = $this->dec_to_hex($binary_code);
@@ -2315,7 +2315,7 @@ class DNS1D {
         $bitval = 1;
         $len = strlen($hex);
         for ($pos = ($len - 1); $pos >= 0; --$pos) {
-            $dec = bcadd($dec, bcmul(hexdec($hex{$pos}), $bitval));
+            $dec = bcadd($dec, bcmul(hexdec($hex[$pos]), $bitval));
             $bitval = bcmul($bitval, 16);
         }
         return $dec;
@@ -2419,7 +2419,7 @@ class DNS1D {
     }
 
     protected function setStorPath($path) {
-        $this->store_path = $path;
+        $this->store_path = rtrim($path, '/' . DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
         return $this;
     }
 
