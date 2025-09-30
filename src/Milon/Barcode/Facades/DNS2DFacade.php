f4fa9b86224d72ff7ccf1c18250c860a03851a22
erikn69 <erikn_69@hotmail.com>
2025-09-30 11:56:49 -0500

    Support background color, transparent as default

--- src/Milon/Barcode/Facades/DNS2DFacade.php
+++ src/Milon/Barcode/Facades/DNS2DFacade.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static string getBarcodeSVG(string $code, string $type, int $w = 3, int $h = 3, string $color = 'black')
  * @method static string getBarcodeHTML(string $code, string $type, int $w = 10, int $h = 10, string $color = 'black')
- * @method static string|false getBarcodePNG(string $code, string $type, int $w = 3, int $h = 3, array $color = [0, 0, 0],)
- * @method static string|false getBarcodePNGPath(string $code, string $type, int $w = 2, int $h = 30, array $color = [0, 0, 0])
+ * @method static string|false getBarcodePNG(string $code, string $type, int $w = 3, int $h = 3, array $color = [0, 0, 0], ?array $bgcolor = null)
+ * @method static string|false getBarcodePNGPath(string $code, string $type, int $w = 2, int $h = 30, array $color = [0, 0, 0], ?array $bgcolor = null)
  * @method static \Milon\Barcode\DNS2D setStorPath(string $path)
  */
 class DNS2DFacade extends Facade
